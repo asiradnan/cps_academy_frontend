a2fa88b73b824c68663b64475cc055e993f4a063
asiradnan <asiradnan23@gmail.com>
2025-10-05 15:05:40 +0600

    class video

--- app/courses/[course]/[topic]/[class]/page.js
+++
@@ -1,9 +0,0 @@
-export default function Topic({ params }) {
-    return (
-        <>
-            <h1>{params.topic}</h1>
-            <h2>{params.class}</h2>
-        </>
-
-    );
-}
--- app/courses/[course]/[topic]/page.js
+++ app/courses/[course]/[topic]/page.js
@@ -1,8 +1,11 @@
 "use client"
 
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
-export default function Topic({ params }) {
+export default function Topic() {
+    const params = useParams()
+    console.log(params) 
     const [topic, setTopic] = useState({})
     async function fetchTopic() {
         const response = await fetch(`https://strapicpsacademy-production.up.railway.app/api/topics/${params.topic}?populate=classes`,
@@ -24,9 +27,14 @@ export default function Topic({ params }) {
             <h1>Topic:</h1>
             <h1>{topic.title}</h1>
             <ol>
-                {topic.classes && topic.classes.map((x, index) => {
+                {topic.classes && topic.classes.map((cls, index) => {
                     return (
-                        <li key={index}> {x.title} </li>
+                        <>
+                            <li key={index}> {cls.title} </li>
+                            <iframe width="80%" height="auto" src={cls.video_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                        </>
+
+
                     )
                 })}
             </ol>
