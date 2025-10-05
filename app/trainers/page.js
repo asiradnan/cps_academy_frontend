307b10341bc3a62e8bc54abfa032404e882e9cf3
asiradnan <asiradnan23@gmail.com>
2025-10-05 19:41:06 +0600

    add trainers page

---
+++ app/trainers/page.js
@@ -0,0 +1,8 @@
+export default function Trainers() {
+    return (
+        <>
+            <h1>Trainers Page</h1>
+            <p>Welcome to the Trainers Page. Here you can find information about our trainers, their expertise, and how to get in touch with them.</p>
+        </>
+    )
+}
