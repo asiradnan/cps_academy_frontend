20682f34586d5713145733fe73769bf951eaa6cb
asiradnan <asiradnan23@gmail.com>
2025-10-06 14:34:42 +0600

    smm can edit trainers

--- app/trainers/page.js
+++ app/trainers/page.js
@@ -6,6 +6,7 @@ import Image from "next/image"
 
 export default function Trainers() {
     const [trainers, setTrainers] = useState([])
+    const [user, setUser] = useState(null)
     async function fetchTrainers() {
         const response = await fetch('https://strapicpsacademy-production.up.railway.app/api/trainers?populate=*')
         const data = await response.json()
@@ -15,14 +16,21 @@ export default function Trainers() {
 
     useEffect(() => {
         fetchTrainers()
+        setUser(localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null)
     }, [])
     if (!trainers) return <div>Loading...</div>
     return (
         <>
             <h1>Trainers Page</h1>
+            {user && user.role.name === 'Social Media Manager' && (
+                <Link href="/trainers/add">Add Trainer</Link>
+            )}
             {trainers && trainers.map((trainer, idx) => {
                 return (
                     <div key={idx}>
+                        {user && user.role.name === 'Social Media Manager' && (
+                            <Link href="/trainers/edit">Edit</Link>
+                        )}
                         <h3>{trainer.name}</h3>
                         <Image src={`https://strapicpsacademy-production.up.railway.app${trainer.profilePicture.url}`} alt={trainer.name} width={200} height={200} />
                         <Link href={`https://codeforces.com/profile/${trainer.codeforcesHandle}`}>{trainer.codeforcesHandle}</Link >
@@ -33,8 +41,8 @@ export default function Trainers() {
                     </div>
 
                 )
-            })
-            }
+            })}
+            
         </>
     )
 }
